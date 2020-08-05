437e72389843f3cb7b58f2e7f5a0ab3c43bb42c4
dalejoroc11 <diegorojas279@hotmail.com>
2020-08-05 00:57:15 +0000

    fix task one twwo four

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+# modify ssh config file
+exec {'/etc/ssh/ssh_config':
+path     => '/bin',
+command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+provider => 'shell',
+}
