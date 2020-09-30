cacc4b2bf2734687cd64c805391b325f0996b755
dalejoroc11 <dalejoroc11@github.com>
2020-09-30 16:52:25 +0000

    fix README AND TASK ZERO

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# Fix an issue when Wordpress is trying initializa
+
+exec { 'fix-wordpress':
+  command => "/bin/sed -i /var/www/html/wp-settings.php \
+  -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
+}
+
+
+
